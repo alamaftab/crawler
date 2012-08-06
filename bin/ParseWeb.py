543a9707152a14d58915d2afb4838d0b1d1c547c
unknown <aalam0@USHOFLT320975.kih.kmart.com>
2012-08-06 13:58:10 -0500

    new changes to put function and parameters.

--- bin/ParseWeb.py
+++ bin/ParseWeb.py
@@ -1,25 +1,42 @@
+#!/usr/bin/python
+
 from BeautifulSoup import BeautifulSoup
 import re
 import urllib2
 
-f = open("test1.txt", 'w')
+configFile='c:/aftab/python/crawler/config/crawler.cfg'
+execfile('c:/aftab/python/crawler/config/crawler.cfg')
+print crawlerData
 
-url = 'http://finance.yahoo.com/q/op?s=GRPN&m=2012-08'
-response = urllib2.urlopen(url)
-html = response.read()
 
-soup = BeautifulSoup(html)
 
-for tablink in soup.findAll("td", {'class':re.compile("yfnc"),'nowrap':"nowrap" }):
-    row=''
-    for tt in tablink.parent.findAll("td"):
-        print tt.text
-        row = row  + '|' + tt.text.replace(',','')
-    print "------"
-    print row
-    f.write(row + "\n")
+#Read input from crawlerData loop for every stock and innner loop for every date. Also download history.
 
-f.close()
+stockSymbol='GRPN'
+expDt='2012-08'
+
+def parse_yahoo_option_url(stockSym_P, expdt_P,outputFilePath_P):
+
+    url = 'http://finance.yahoo.com/q/op?s=' + stockSym_P + '&m=' + expdt_P
+    response = urllib2.urlopen(url)
+    html = response.read()
+
+    soup = BeautifulSoup(html)
+    outputFile=outputFilePath_P + "/out/test1.txt"
+    f = open(outputFile, 'w')
+    for tablink in soup.findAll("td", {'class':re.compile("yfnc"),'nowrap':"nowrap" }):
+        row=''
+        for tt in tablink.parent.findAll("td"):
+            print tt.text
+            row = row  + '|' + tt.text.replace(',','')
+        print "------"
+        print row
+        f.write(row + "\n")
+
+    f.close()
+
+
+parse_yahoo_option_url( stockSymbol, expDt,   crawlerData)
     
 
 
@@ -30,7 +47,7 @@ f.close()
                                       
                                          
 
-
+    
 
 
 

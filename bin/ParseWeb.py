7e3e41646d8de14170c8b3035c7ae1c4644d409c
unknown <aalam0@USHOFLT320975.kih.kmart.com>
2012-08-08 22:57:33 -0500

    Changes for fixing filename

--- bin/ParseWeb.py
+++ bin/ParseWeb.py
@@ -3,26 +3,34 @@
 from BeautifulSoup import BeautifulSoup
 import re
 import urllib2
+import datetime
+import time
 
 configFile='c:/aftab/python/crawler/config/crawler.cfg'
 execfile('c:/aftab/python/crawler/config/crawler.cfg')
 print crawlerData
 
-
+def read_input_file(inputFile_P):
+    inputDict={}
+    fp=open(inputFile_P)
+    for i,st in enumerate(fp):
+        print i
+        print st
 
 #Read input from crawlerData loop for every stock and innner loop for every date. Also download history.
 
-stockSymbol='GRPN'
-expDt='2012-08'
+stockSymbol='SHLD'
+expDt='2012-09'
 
 def parse_yahoo_option_url(stockSym_P, expdt_P,outputFilePath_P):
 
-    url = 'http://finance.yahoo.com/q/op?s=' + stockSym_P + '&m=' + expdt_P
+    url = 'http://finance.yahoo.com/q/op?s=' + stockSym_P  + '&m=' + expdt_P
     response = urllib2.urlopen(url)
     html = response.read()
 
     soup = BeautifulSoup(html)
-    outputFile=outputFilePath_P + "/out/test1.txt"
+    fileSuffix = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+    outputFile=outputFilePath_P + "/out/" + stockSym_P  + "_" + expdt_P +  "_"  + fileSuffix  + ".txt"
     f = open(outputFile, 'w')
     for tablink in soup.findAll("td", {'class':re.compile("yfnc"),'nowrap':"nowrap" }):
         row=''

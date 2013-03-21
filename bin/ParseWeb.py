cfb374aeba857f41c0ce607fb0f34fedc0f3221f
unknown <aalam0@USHOFLT320975.kih.kmart.com>
2013-03-21 11:34:09 -0500

    hist load function

--- bin/ParseWeb.py
+++ bin/ParseWeb.py
@@ -12,6 +12,8 @@ sys.path.append(crawlerHome +"/lib")
 
 import ParseYahooFinanceWebLib
 
+###################################
+## Declare variables
 inputDict={}
 
 ##Main program begin from here##
@@ -20,6 +22,9 @@ inputDict=ParseYahooFinanceWebLib.read_input_file(crawlerData + '/in/stock.txt')
 for key in inputDict.keys():
     for dt in inputDict[key]:
         ParseYahooFinanceWebLib.parse_yahoo_option_url( key, dt,   crawlerData)
+#Historical download.
+    ParseYahooFinanceWebLib.parse_yahoo_history_url(key,crawlerData)
+        
         
 
 
